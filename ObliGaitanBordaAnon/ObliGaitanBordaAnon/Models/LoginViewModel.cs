c6353e7a342c3c26adf945b7e3ee4cc37eadd51f
feli21a <felianon14@gmail.com>
2024-06-11 15:12:07 -0300

    cambios login

--- ObliGaitanBordaAnon/ObliGaitanBordaAnon/Models/LoginViewModel.cs
+++ ObliGaitanBordaAnon/ObliGaitanBordaAnon/Models/LoginViewModel.cs
@@ -6,10 +6,8 @@ namespace ObliGaitanBordaAnon.Models
     public class LoginViewModel
     {
         [Required(ErrorMessage = "El campo Email es obligatorio.")]
-        [EmailAddress(ErrorMessage = "El formato del Email no es válido.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "El campo Contraseña es obligatorio.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
