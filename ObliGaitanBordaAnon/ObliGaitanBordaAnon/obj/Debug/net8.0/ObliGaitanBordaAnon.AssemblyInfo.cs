ec517eeaa0c8b1be896d6d12c8c20e2b4a99bf32
feli21a <felianon14@gmail.com>
2024-06-21 01:59:47 -0300

    muchos cambios

--- ObliGaitanBordaAnon/ObliGaitanBordaAnon/obj/Debug/net8.0/ObliGaitanBordaAnon.AssemblyInfo.cs
+++ ObliGaitanBordaAnon/ObliGaitanBordaAnon/obj/Debug/net8.0/ObliGaitanBordaAnon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObliGaitanBordaAnon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da73990b23fbd76f4810ccfdd378799373bbd9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89923c5545d2fe0f38a316af6f84e3fe6a1ef1b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObliGaitanBordaAnon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObliGaitanBordaAnon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
